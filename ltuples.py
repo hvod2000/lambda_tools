501d50dd52c5687dea3e5fda22e59b5b475e71a8
Uladzislau Khamkou <khamkou@tuta.io>
2021-10-05 10:11:38 +0300

    tuples: add tuples of size up to 64

--- ltuples.py
+++ ltuples.py
@@ -5,3 +5,8 @@ CLASSIC_FIRST = l("&(&&2)!1")
 CLASSIC_SECOND = l("&(&&1)!1")
 POLISH_FIRST, POLISH_SECOND = CLASSIC_SECOND, CLASSIC_FIRST
 FIRST, SECOND = POLISH_FIRST, POLISH_SECOND
+
+for i in range(1, 64+1):
+    prefix = '&' * i
+    suffix = ''.join(f'{j + 2}!' for j in range(i))
+    exec(f"T{i} = l('{prefix + ' &' + suffix + ' 1'}')")
