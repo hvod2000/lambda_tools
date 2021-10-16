39c91654a716c1d68e2b27d023a5082f6b7a67d8
Uladzislau Khamkou <khamkou@tuta.io>
2021-10-16 10:31:19 +0300

    tuples: define tuples in lambdas instead of python

--- ltuples.py
+++ ltuples.py
@@ -1,4 +1,5 @@
 from lambdas import string_to_lambda_expression as l
+from lambdas import reduce_lambda_to_beta_normal_form as b
 
 PAIR = l("&&&2!3!1")
 CLASSIC_FIRST = l("&(&&2)!1")
@@ -6,7 +7,10 @@ CLASSIC_SECOND = l("&(&&1)!1")
 POLISH_FIRST, POLISH_SECOND = CLASSIC_SECOND, CLASSIC_FIRST
 FIRST, SECOND = POLISH_FIRST, POLISH_SECOND
 
-for i in range(1, 64+1):
-    prefix = '&' * i
-    suffix = ''.join(f'{j + 2}!' for j in range(i))
-    exec(f"T{i} = l('{prefix + ' &' + suffix + ' 1'}')")
+SHEME = l('&&1!2')
+SHEME_BUILDER = l('&&&(&2!1!3)!3')
+TUPLE_BUILDER = l('&(&1)!1')
+T = {}
+for i in range(64 + 1):
+    T[i] = b(l(f"({SHEME})!{TUPLE_BUILDER}"))
+    SHEME = b(l(f"({SHEME})!{SHEME_BUILDER}"))
